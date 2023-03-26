6649cdcfc6fed9d9ead88609c246cdd75050a31d
Lexey9105 <umpp120@gmail.com>
2023-03-26 21:06:12 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
 
+    }
+
+    private static void task1() {
+        String firstName="Ivanov";
+        String middleName="Ivan";
+        String lastName="Ivanovich";
+        String fullName=firstName+" "+middleName+" "+lastName;
+        System.out.println("Ф.И.О сотрудника "+fullName);
+    }
+    private static void task2() {
+        String firstName="Ivanov";
+        String middleName="Ivan";
+        String lastName="Ivanovich";
+        String fullName=firstName+" "+middleName+" "+lastName;
+        System.out.println(fullName.toUpperCase());
+    }
+    private static void task3() {
+        String fullName="Иванов Семён Семёнович";
+        fullName=fullName.replace('ё','е');
+        System.out.println(fullName);
     }
 }
